ff44040b630592e86daf5bef44bb310986e63ee4
Bakradze <nikabakradze10@gmail.com>
2026-08-31 22:38:51 +0400

    Update board.js

--- routes/board.js
+++ routes/board.js
@@ -6,7 +6,7 @@ const verifyToken = require('../middleware/verifyToken');
 router.get('/', verifyToken, boardController.getBoards);
 router.get('/:id', verifyToken, boardController.getBoardById);
 router.post('/', verifyToken, boardController.createBoard);
-router.post('/column', verifyToken, boardController.addColumn);
+router.post('/:boardId/columns', verifyToken, boardController.addColumn);
 router.put('/:id', verifyToken, boardController.updateBoard);
 router.delete('/:id', verifyToken, boardController.deleteBoard);
 
